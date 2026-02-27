ba21b8967c8b368b3381a1f5cfc72ce986a027b8
PilkeySEK <PilkeySEK@gmx.de>
2026-02-27 16:46:32 +0100

    Add MessageType enum

--- fluxer-gateway/src/model/object/message.rs
+++ fluxer-gateway/src/model/object/message.rs
@@ -1,7 +1,87 @@
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 use time::OffsetDateTime;
 
 use crate::model::{object::user::UserPartialResponse, snowflake::Snowflake};
+
+#[derive(Serialize_repr, Deserialize_repr, Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(u8)]
+pub enum MessageType {
+    /// Deletable: `true`
+    Default = 0,
+    /// Deletable: `false`
+    RecipientAdd = 1,
+    /// Deletable: `false`
+    RecipientRemove = 2,
+    /// Deletable: `false`
+    Call = 3,
+    /// Deletable: `false`
+    ChannelNameChange = 4,
+    /// Deletable: `false`
+    ChannelIconChange = 5,
+    /// Deletable: `true`
+    ChannelPinnedMessage = 6,
+    /// Deletable: `true`
+    UserJoin = 7,
+    /// Deletable: `true`
+    GuildBoost = 8,
+    /// Deletable: `true`
+    GuildBoostTier1 = 9,
+    /// Deletable: `true`
+    GuildBoostTier2 = 10,
+    /// Deletable: `true`
+    GuildBoostTier3 = 11,
+    /// Deletable: `true`
+    ChannelFollowAdd = 12,
+    /// Deletable: `true`
+    GuildDiscoveryDisqualified = 14,
+    /// Deletable: `true`
+    GuildDiscoveryRequalified = 15,
+    /// Deletable: `true`
+    GuildDiscoveryGracePeriodInitialWarning = 16,
+    /// Deletable: `true`
+    GuildDiscoveryGracePeriodFinalWarning = 17,
+    /// Deletable: `true`
+    ThreadCreated = 18,
+    /// Deletable: `true`
+    Reply = 19,
+    /// Deletable: `true`
+    ChatInputCommand = 20,
+    /// Deletable: `false`
+    ThreadStarterMessage = 21,
+    /// Deletable: `true`
+    GuildInviteReminder = 22,
+    /// Deletable: `true`
+    ContextMenuCommand = 23,
+    /// Deletable: `true` (only deletable by members with `MANAGE_MESSAGES` permission)
+    AutoModerationAction = 24,
+    /// Deletable: `true`
+    RoleSubscriptionPurchase = 25,
+    /// Deletable: `true`
+    InteractionPremiumUpsell = 26,
+    /// Deletable: `true`
+    StageStart = 27,
+    /// Deletable: `true`
+    StageEnd = 28,
+    /// Deletable: `true`
+    StageSpeaker = 29,
+    /// Deletable: `true`
+    StageTopic = 31,
+    /// Deletable: `true`
+    GuildApplicationPremiumSubscription = 32,
+    /// Deletable: `true`
+    GuildIncidentAlertModeEnabled = 36,
+    /// Deletable: `true`
+    GuildIncidentAlertModeDisabled = 37,
+    /// Deletable: `true`
+    GuildIncidentReportRaid = 38,
+    /// Deletable: `true`
+    GuildIncidentReportFalseAlarm = 39,
+    /// Deletable: `true`
+    PurchaseNotification = 44,
+    /// Deletable: `true`
+    PollResult = 46,
+}
 
 // Not officially documented?
 // https://docs.fluxer.app/api-reference/channels/send-a-message ?
@@ -13,7 +93,7 @@ pub struct MessageResponse {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub webhook_id: Option<Snowflake>,
     #[serde(rename = "type")]
-    pub r#type: i32,
+    pub r#type: MessageType,
     pub flags: i32,
     pub content: String,
     #[serde(with = "time::serde::iso8601")]
